f1a752b1e1a057995b1aaba11928ae03920f6608
SudarshanaRao <halukubi@denipl.net>
2026-01-29 11:59:11 +0530

    Coming Sooon Testing

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -15,7 +15,7 @@ export function ComingSoon({ onComplete }: ComingSoonProps) {
     const calculateTimeLeft = useCallback(() => {
     const now = new Date();
     const target = new Date();
-    target.setHours(11, 58, 0, 0);
+    target.setHours(12, 15, 0, 0);
     if (target < now) target.setDate(target.getDate());
 
     const difference = target.getTime() - now.getTime();
